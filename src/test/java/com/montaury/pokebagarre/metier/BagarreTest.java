98b681a71aa793e5d1336715790e765106352ae0
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2025-03-26 10:43:48 +0100

    tests unitaires de la methode demarrer finis

--- src/test/java/com/montaury/pokebagarre/metier/BagarreTest.java
+++ src/test/java/com/montaury/pokebagarre/metier/BagarreTest.java
@@ -2,11 +2,14 @@ package com.montaury.pokebagarre.metier;
 
 import com.montaury.pokebagarre.erreurs.ErreurMemePokemon;
 import com.montaury.pokebagarre.erreurs.ErreurPokemonNonRenseigne;
+import com.montaury.pokebagarre.erreurs.ErreurRecuperationPokemon;
 import com.montaury.pokebagarre.webapi.PokeBuildApi;
 import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 
+import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -94,23 +97,68 @@ class BagarreTest {
         Throwable thrown = assertThrows(ErreurMemePokemon.class, () -> bagarre.demarrer(nomPremierPokemon, nomSecondPokemon));
 
         //THEN
-        assertThat(thrown).isInstanceOf(ErreurMemePokemon.class);
+        assertThat(thrown).isInstanceOf(ErreurMemePokemon.class).hasMessage("Impossible de faire se bagarrer un pokemon avec lui-meme");
     }
 
     @Test
     public void devrait_determiner_le_vainqueur_correctement() {
         //GIVEN
-        Pokemon pikachu = Mockito.mock(Pokemon.class);
-        Pokemon bulbizarre = Mockito.mock(Pokemon.class);
+        when(mockApi.recupererParNom("Pikachu")).thenReturn(CompletableFuture.
+                completedFuture(new Pokemon("Pikachu", "url1", new Stats(1,2))));
+        when(mockApi.recupererParNom("Bulbizarre")).thenReturn(CompletableFuture.
+                completedFuture(new Pokemon("Bulbizarre", "url2", new Stats(2,2))));
 
         //WHEN
-        when(mockApi.recupererParNom("Pikachu")).thenReturn(CompletableFuture.completedFuture(pikachu));
-        when(mockApi.recupererParNom("Bulbizarre")).thenReturn(CompletableFuture.completedFuture(bulbizarre));
-        when(pikachu.estVainqueurContre(bulbizarre)).thenReturn(true);
-
-        Pokemon vainqueur = bagarre.demarrer("Pikachu", "Bulbizarre").join();
+        var futurVainqueur = bagarre.demarrer("Pikachu", "Bulbizarre");
 
         //THEN
-        assertThat(vainqueur).isEqualTo(pikachu);
+        assertThat(futurVainqueur)
+                .succeedsWithin(Duration.ofSeconds(2))
+                .satisfies(pokemon -> {
+                        assertThat(pokemon.getNom()).isEqualTo("Bulbizarre");
+                        assertThat(pokemon.getUrlImage()).isEqualTo("url2");
+                        assertThat(pokemon.getStats().getAttaque()).isEqualTo(2);
+                        assertThat(pokemon.getStats().getDefense()).isEqualTo(2);
+                });
+    }
+
+    @Test
+    public void devrait_echouer_si_erreur_api_premier_pokemon() {
+        //GIVEN
+        when(mockApi.recupererParNom("Pikachu")).thenReturn(CompletableFuture.
+                failedFuture(new ErreurRecuperationPokemon("Pikachu")));
+        when(mockApi.recupererParNom("Bulbizarre")).thenReturn(CompletableFuture.
+                completedFuture(new Pokemon("Bulbizarre", "url2", new Stats(3,4))));
+
+        //WHEN
+        var futurVainqueur = bagarre.demarrer("Pikachu", "Bulbizarre");
+
+        //THEN
+        assertThat(futurVainqueur)
+                .failsWithin(Duration.ofSeconds(2))
+                .withThrowableOfType(ExecutionException.class)
+                .havingCause()
+                .isInstanceOf(ErreurRecuperationPokemon.class)
+                .withMessage("Impossible de recuperer les details sur 'Pikachu'");
+    }
+
+    @Test
+    public void devrait_echouer_si_erreur_api_deuxieme_pokemon() {
+        //GIVEN
+        when(mockApi.recupererParNom("Pikachu")).thenReturn(CompletableFuture.
+                completedFuture(new Pokemon("Pikachu", "url1", new Stats(3,4))));
+        when(mockApi.recupererParNom("Bulbizarre")).thenReturn(CompletableFuture.
+                failedFuture(new ErreurRecuperationPokemon("Bulbizarre")));
+
+        //WHEN
+        var futurVainqueur = bagarre.demarrer("Pikachu", "Bulbizarre");
+
+        //THEN
+        assertThat(futurVainqueur)
+                .failsWithin(Duration.ofSeconds(2))
+                .withThrowableOfType(ExecutionException.class)
+                .havingCause()
+                .isInstanceOf(ErreurRecuperationPokemon.class)
+                .withMessage("Impossible de recuperer les details sur 'Bulbizarre'");
     }
 }
